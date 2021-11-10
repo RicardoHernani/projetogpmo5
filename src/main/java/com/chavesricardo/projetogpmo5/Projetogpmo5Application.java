1135f45b0b01b36a3f87b11e0a4146e30533da43
RicardoHernani <ricardohernani@yahoo.com.br>
2021-11-09 23:39:47 -0300

    Projeto criado

---
+++ src/main/java/com/chavesricardo/projetogpmo5/Projetogpmo5Application.java
@@ -0,0 +1,13 @@
+package com.chavesricardo.projetogpmo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetogpmo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetogpmo5Application.class, args);
+	}
+
+}
